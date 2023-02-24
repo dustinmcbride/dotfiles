eca37774808798e57115ccd4265d15fcd3448b67
DUSTIN MCBRIDE <dustinbm@gmail.com>
2023-02-24 14:57:41 -0800

    adding install and re org

---
+++ install.mjs
@@ -0,0 +1,42 @@
+
+// const fs = require('fs')
+import path from 'path'
+import os from "os"
+import { cp } from 'node:fs/promises';
+import fs from 'fs'
+
+const userHomeDir = os.homedir();
+
+const tracekdConfigs = [
+  '.config/fish',
+  '.config/karabiner',
+  '.config/omf',
+  '.config/yabai',
+  'starship.toml',
+  '.docker',
+  '.test'
+]
+
+
+const homeConfigDir = path.join(userHomeDir, '.config')
+
+if (!fs.existsSync(homeConfigDir)){
+  fs.mkdirSync(homeConfigDir);
+}
+
+const alreadyExistsInHomeDir = tracekdConfigs.filter(p =>   
+  fs.existsSync(path.join(userHomeDir, p))
+)
+
+console.log("The following are already install in the user's")
+console.log("home directory and will not be installed")
+console.log(alreadyExistsInHomeDir)
+
+const configToCreateSymLinksFor = tracekdConfigs.filter(p =>
+  !alreadyExistsInHomeDir.includes(p)
+  )
+
+const configDir = (p) => path.join(userHomeDir, p)
+const homeDir = (p) => path.join(process.cwd(), 'home', p)
+
+configToCreateSymLinksFor.forEach(p => fs.symlinkSync(homeDir(p), configDir(p)))
